2f0b26406f328ae92bd67ec340a3b377d176b7af
Linus Lundahl <lundahl.linus@gmail.com>
2012-10-15 13:08:32 +0200

    Create an install script.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Instructions
 
 This is a blank starter theme for Drupal 7.
 
-1. Rename go_blank.info to your_theme_name.info.
-2. Find and replace GO_BLANK with your_theme_name.
+1. Run `$ ./install.sh your_theme_name`.
+2. Start creating your theme.
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+NAME=$1
+mv go_blank.info $NAME.info
+perl -pi -w -e 's/GO_BLANK/'$NAME'/g;' *.php
+perl -pi -w -e 's/GO_BLANK/'$NAME'/g;' javascripts/*.js
+rm install.sh
+rm README.md
+rm fonts/README.md
+echo "New theme named $NAME created."
