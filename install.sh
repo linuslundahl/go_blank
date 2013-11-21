3baa8f423d6e17c00a0d02704d7f805310071710
Linus Lundahl <lundahl.linus@gmail.com>
2013-11-21 21:26:35 +0100

    Update install script.

--- install.sh
+++ install.sh
@@ -2,10 +2,15 @@
 NAME=$1
 mv go_blank.info $NAME.info
 perl -pi -w -e 's/GO_BLANK/'$NAME'/g;' *.php
+perl -pi -w -e 's/GO_BLANK/'$NAME'/g;' *.info
+perl -pi -w -e 's/GO_BLANK/'$NAME'/g;' *.json
 perl -pi -w -e 's/GO_BLANK/'$NAME'/g;' javascripts/*.js
 rm install.sh
 rm README.md
 rm images/sprite/README.md
 rm fonts/README.md
 rm -rf .git
+git init
+git add .
+git commit -m "Initial commit."
 echo "New theme named $NAME created."
